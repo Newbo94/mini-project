70aaca337180519fe172df77e24f0f0cb735d78e
Jan-Nilas Gerth <janx1644@edu.eal.dk>
2018-09-07 18:25:46 +0200

    singleproduct build

---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="product">
+        <figure class="product__item">
+            <figcaption class="product__price">{{title}} </figcaption>
+            <img class="product__image" :src="img" alt="test">
+        </figure>
+    
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "signleproduct",
+    props: ["title", "price", "description", "img"],
+   
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,24 @@
         </section>
 
         <hr>
+<div class="wrappper">
+        <singleproduct :img="require('@/assets/images/standard/item1.png')" title="£89,99"></singleproduct> 
+        <singleproduct :img="require('@/assets/images/standard/item2.png')" title="£89,99"></singleproduct> 
+        <singleproduct :img="require('@/assets/images/standard/item3.png')" title="£89,99"></singleproduct> 
+        <singleproduct :img="require('@/assets/images/standard/item4.png')" title="£89,99"></singleproduct> 
+        <singleproduct :img="require('@/assets/images/standard/item5.png')" title="£89,99"></singleproduct> 
+        <singleproduct :img="require('@/assets/images/standard/item6.png')"title="£89,99" ></singleproduct> 
+</div>
     </div>
 </template>
 
 <script>
-
+import singleproduct from "@/components/SingleProduct.vue";
 
 export default {
-  name: 'home',
+  name: 'home', 
+  components: {
+    singleproduct
+  }
 };
 </script>
