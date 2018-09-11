91f1f5b3eeb4f50acdef1cc016278b139e82fabc
Jan-Nilas Gerth <janx1644@edu.eal.dk>
2018-09-11 11:32:57 +0200

    tabs & product styling

--- src/components/SingleProduct.vue
+++ src/components/SingleProduct.vue
@@ -3,13 +3,21 @@
     <div class="product">
         <figure class="product__item">
             <figcaption class="product__price">{{price}} </figcaption>
-            <img class="product__image" :src="img" alt="test">
+              <img class="product__thumbnail product__thumbnail--hovered" :src="thumb1" alt="test">
+            <img class="product__thumbnail product__thumbnail--hovered" :src="thumb2" alt="test">
+            <img class="product__image  " :src="img" alt="test">
+          
+
         <div class="product__content product__content--hovered">
+            <font-awesome-icon icon="info-circle" class="product__thumbicon" size="4x"></font-awesome-icon>
+
+          
                 <h5>{{title}}</h5>
                 <p>{{description}}</p>
-              <font-awesome-icon icon="shopping-cart" class="product__icon"></font-awesome-icon>
-              <font-awesome-icon icon="heart" class="product__icon"></font-awesome-icon>
-              <font-awesome-icon icon="times" class="product__icon"></font-awesome-icon>
+              <font-awesome-icon icon="shopping-cart" class="product__icon" size="1x"></font-awesome-icon>
+              <font-awesome-icon icon="heart" class="product__icon" size="1x"></font-awesome-icon>
+              <font-awesome-icon icon="times" class="product__icon" size="1x"></font-awesome-icon>
+
         </div>
         </figure>
     
@@ -20,7 +28,7 @@
 <script>
 export default {
     name: "SingleProduct",
-    props: ["title", "price", "description", "img"],
+    props: ["title", "price", "description", "img", "thumb1", "thumb2"],
    
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,24 @@
         </section>
 
         <hr>
-            <div class="productwrapper">
+
+       
+
+<div id="tabs">
+    <div class="tabs">
+        <a v-on:click="activetab=1" v-bind:class="[ activetab === 1 ? 'active' : '' ]">Popular</a>
+        <a v-on:click="activetab=2" v-bind:class="[ activetab === 2 ? 'active' : '' ]">New arrivals</a>
+        <a v-on:click="activetab=3" v-bind:class="[ activetab === 3 ? 'active' : '' ]">Best sellers</a>
+        <a v-on:click="activetab=4" v-bind:class="[ activetab === 4 ? 'active' : '' ]">Sepcial offer</a>
+        <a v-on:click="activetab=5" v-bind:class="[ activetab === 5 ? 'active' : '' ]">Coming soon</a>
+    </div>
+
+    <div class="content">
+        <div v-if="activetab === 1" class="tabcontent">
+              <div class="productwrapper">
                     <singleproduct :img="require('@/assets/images/standard/item2.png')" price="£89,99" title="Womens burnt orange casual tee  £29.95" description="Classic casual t-shirt for women on the move. 
             100% cotton.
-            A hard-wearing sports t-shirt with microvent technology." ></singleproduct> 
+            A hard-wearing sports t-shirt with microvent technology." :thumb1="require('@/assets/images/standard/thumb-back.png')" :thumb2="require('@/assets/images/standard/thumb-front.png')" ></singleproduct> 
                     <singleproduct :img="require('@/assets/images/standard/item1.png')" price="£89,99" title="Womens burnt orange casual tee  £29.95" description="Classic casual t-shirt for women on the move. 
             100% cotton.
             A hard-wearing sports t-shirt with microvent technology." ></singleproduct> 
@@ -26,6 +40,23 @@
             100% cotton.
             A hard-wearing sports t-shirt with microvent technology." ></singleproduct> 
             </div>
+        </div>
+        <div v-if="activetab === 2" class="tabcontent">
+            Content for tab two
+        </div>
+        <div v-if="activetab === 3" class="tabcontent">
+            Content for tab three
+        </div>
+           <div v-if="activetab === 4" class="tabcontent">
+            Content for tab four
+        </div>
+           <div v-if="activetab ===5" class="tabcontent">
+            Content for tab five
+        </div>
+    </div>
+</div>
+
+        
         <hr>
             <section class="lookbookwrapper">
               <lookbookpreview title="Men’s lookbook" text="Lorem ipsum dolor sit amet eras facilisis
@@ -38,6 +69,8 @@ Elementum metus facilisis ut phasellu." :img="require('@/assets/images/standard/
 consectetur adipiscing elit lor, integer lorem consecteur dignissim laciniqui.
 Elementum metus facilisis ut phasellu." :img="require('@/assets/images/standard/photo_silhouette.png')"></lookbookpreview>
             </section>
+
+            <tab></tab>
     </div>
 </template>
 
@@ -47,10 +80,18 @@ import lookbookpreview from '@/components/LookBookPreview.vue';
 
 export default {
   name: "home",
+  data() {
+            return {
+ activetab: 1 
+            };
+            },
+
   components: {
     singleproduct,
-    lookbookpreview
+    lookbookpreview,
+    
 
-  }
+  },
+    
 };
 </script>
